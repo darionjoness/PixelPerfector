942402a9382048e475ea07cfbf88af6cea921027
darionjoness <101945727+darionjoness@users.noreply.github.com>
2023-11-22 17:06:44 -0600

    changed gpt prompt

--- backend/controllers/gptControllers.js
+++ backend/controllers/gptControllers.js
@@ -30,7 +30,7 @@ const getGptResponse = asyncHandler(async (req, res) => {
             {
                 role: 'user',
                 content: [
-                    { type: 'text', text: 'Can you analyze my web design and give me feedback on anything I can improve!' },
+                    { type: 'text', text: 'Can you analyze my web design and give me feedback on anything I can improve! Can you also split the answer up into the different categories you analyzed and dont have any boilerplate for the answer, just give me the analysis.' },
                     {
                         type: 'image_url',
                         image_url: {
