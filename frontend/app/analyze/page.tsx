4edda92d1cfd58404a9a6074da38133d984b5e03
darionjoness <101945727+darionjoness@users.noreply.github.com>
2023-11-28 15:04:53 -0600

    added session expired check

--- frontend/app/analyze/page.tsx
+++ frontend/app/analyze/page.tsx
@@ -1,19 +1,29 @@
 'use client';
 import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation';
 
 const AnalyzePage =  () => {
+
+    const router = useRouter()
 
     useEffect(() => {
 
         const fetchProfile = async () => {
             try {
                 const res = await fetch('/api/api/users/profile')
+
+                if(!res.ok){
+                    if(res.status === 401){
+                        localStorage.removeItem('user')
+                        router.push('/login')
+                    }
+                }
         
                 const data = await res.json()
 
                 console.log(data)
             } catch (error:any) {
-                console.log(error.message)
+                console.log(error)
             }
         }
 
